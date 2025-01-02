6fa8858413cf4e189728d4367eb7b79e62940ec2
beebozy <musahabeeblai@gmail.com>
2025-01-02 08:44:34 -0800

    Add deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
-use serde::Serialize;
-use std::fs::File;
-use std::io::Write;
+use serde::{Serialize, Deserialize};
+use std::fs::{File,OpenOptions};
+use std::io::{Write,Read};
 
-#[derive(Serialize)]
+#[derive(Serialize,Deserialize)]
 struct User{
     age:i32,
     name:String,
@@ -23,6 +23,34 @@ let mut file= File::create("user.json")?;
 file.write_all(json_data.as_bytes())?;
 
 println!("JSON data written to 'user.json' successfully!");
-    Ok(())
 
+
+let mut file= OpenOptions::new().read(true).open("user.json")?;
+
+let mut contents= String::from("");
+
+file.read_to_string(&mut contents)?;
+let deserialized_user : User= serde_json::from_str(&contents).expect("Deserialization failed");
+println!("Deserialized Struct:\nName: {}\nAge: {}\nEmail: {}", deserialized_user.name, deserialized_user.age, deserialized_user.email);
+
+    Ok(())
 }
+
+// fn deserialize(){
+
+//     let json_data = r#"
+//     {
+    
+//     age :15,
+//     name: "alice",
+
+//     email: "alice@xample.co"
+
+
+//     }#";
+
+
+//     let user : User = serde_json::jsond_data.to_string().expect("Deserializaiton failed");
+
+//     println!("")
+// }
